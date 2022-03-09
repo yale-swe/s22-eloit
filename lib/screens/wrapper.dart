3c5286b54f6145dcf4c9f75c56ed97f83c1a0a4f
HarryJain <HarryJain99@gmail.com>
2022-03-09 14:06:23 -0500

    Outlined wrapper for db and homepage with scaffold

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:eloit/screens/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Home();
+  }
+}

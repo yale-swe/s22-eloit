8a291dd8d437ee5fd64f9367d8520954dead72e7
Yofti <43306816+ymilkessa@users.noreply.github.com>
2022-03-28 12:42:49 -0400

    format (#2)
    * Random change just to test
    
    * format main

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:eloit/screens/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();

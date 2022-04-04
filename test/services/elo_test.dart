ef703bfab2223adc5f471cfe85504adafb70f71d
Xinli Hou <houxinli@hotmail.com>
2022-04-03 21:12:43 -0400

    Update elo_test.dart

--- test/services/elo_test.dart
+++ test/services/elo_test.dart
@@ -1,12 +1,22 @@
+import 'package:test/test.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 
+const uid = "abc";
+
 void main() {
-  final instance = FakeFirebaseFirestore();
-  await instance.collection('users').add({
-    'username': 'Bob',
+  const expectedDumpAfterset = '''{
+  "users": {
+    "abc": {
+      "name": "Bob"
+    }
+  }
+}''';
+
+  test('Sets data for a document within a collection', () async {
+    final instance = FakeFirebaseFirestore();
+    await instance.collection('users').doc(uid).set({
+      'name': 'Bob',
+    });
+    expect(instance.dump(), equals(expectedDumpAfterset));
   });
-  final snapshot = await instance.collection('users').get();
-  print(snapshot.docs.length); // 1
-  print(snapshot.docs.first.get('username')); // 'Bob'
-  print(instance.dump());
 }

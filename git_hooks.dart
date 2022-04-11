ed0c8afabb1135c221fbe8748bd13b05ec7d73f4
Xinli Hou <houxinli@hotmail.com>
2022-04-11 18:43:51 -0400

    pre commit test

---
+++ git_hooks.dart
@@ -0,0 +1,25 @@
+import 'package:dart_pre_commit/dart_pre_commit.dart';
+import 'package:git_hooks/git_hooks.dart';
+import 'package:riverpod/riverpod.dart';
+// import 'dart:io';
+
+void main(List<String> arguments) {
+  // ignore: omit_local_variable_types
+  Map<Git, UserBackFun> params = {Git.preCommit: _preCommit};
+  GitHooks.call(arguments, params);
+}
+
+Future<bool> _preCommit() async {
+  // optional: create an IoC-Container for easier initialization of the hooks
+  final container = ProviderContainer();
+
+  // obtain the hooks instance from the IoC with your custom config
+  final hook = await container.read(
+    HooksProvider.hookProvider(
+      const HooksConfig(),
+    ).future,
+  );
+
+  final result = await hook(); // run activated hooks on staged files
+  return result.isSuccess; // report the result
+}
